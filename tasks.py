8b8674f0c09a0e9ce720d684e1f13d8395cce47e
Nahuel Defossé <D3f0@users.noreply.github.com>
2024-08-05 21:26:27 +0300

    Add project task to allow run in a temp virtualenv

--- tasks.py
+++ tasks.py
@@ -1,3 +1,7 @@
+import importlib
+import os
+from pathlib import Path
+import sys
 from invoke import task, Context
 import subprocess
 
@@ -9,12 +13,10 @@ REPO_ROOT = (
 
 
 @task()
-def build(ctx: Context):
-    ctx.run("hatch build", pty=True)
-
-
-@task()
 def test(ctx: Context, debug=False, verbose=False):
+    """Run tests
+
+    It changes to the top level directory of the repo."""
     with ctx.cd(REPO_ROOT):
         args = ""
         if debug:
@@ -27,5 +29,79 @@ def test(ctx: Context, debug=False, verbose=False):
 
 @task()
 def clean_dist(ctx: Context):
+    """Cleans the dist directory"""
     with ctx.cd(REPO_ROOT):
-        ctx.run("dist/*")
+        ctx.run("rm -rf dist/*")
+
+
+@task()
+def upload(ctx: Context, repository="pypi"):
+    ...
+
+
+@task(pre=[clean_dist])
+def build(ctx: Context) -> str:
+    """Builds the whl file with hatch build"""
+    ctx.run("hatch build -t wheel", pty=True, out_stream=sys.stderr)
+    return ctx.run().stdout.strip()
+
+
+@task(
+    help={
+        "install_method": "Allows to use  `wheel`, `source` or  `editable`",
+        "clean": "Flag to disable directory cleanup",
+        "shell": "Override shell",
+    }
+)
+def temp_env(ctx: Context, install_method="wheel", clean=True, shell=None):
+    """Creates a virtual environment"""
+    print("Creating temporary directory...", file=sys.stderr)
+    temp_dir = ctx.run("mktemp -d").stdout.strip()
+    try:
+        print(f"Creating virtualenv in {temp_dir}", file=sys.stderr)
+        ctx.run(f"python3 -m venv {temp_dir}")
+
+        with ctx.cd(temp_dir):
+            if install_method == "wheel":
+                ctx.run(f"source bin/activate && pip install {REPO_ROOT}/dist/*.whl")
+            elif install_method == "editable":
+                ctx.run(f"source bin/activate && pip install -e {REPO_ROOT}")
+            elif install_method == "source":
+                ctx.run(f"source bin/activate && pip install {REPO_ROOT}")
+            else:
+                sys.exit(f"Method {install_method} not recognized")
+
+            print("Opening shell", file=sys.stderr)
+
+            shell = shell or Path(os.getenv("SHELL")).name
+            if shell == "fish":
+                activation_script = "source bin/activate.fish"
+            else:
+                activation_script = "source bin/activate"
+            ctx.run(
+                f"$SHELL -c '{activation_script}; $SHELL'",
+                pty=True,
+                env={"shell": shell},
+            )
+            print("Deleting environment", file=sys.stderr)
+    except Exception as error:
+        print(f"task stopped by: {error}")
+    if clean:
+        ctx.run(f"rm -rf {temp_dir}", pty=True)
+    else:
+        print(f"Temporary environment left {temp_dir}")
+
+
+@task(autoprint=True)
+def print_module_path(ctx: Context, module=""):
+    if not module:
+        return ""
+    try:
+        module = importlib.import_module(name=module)
+    except ImportError as error:
+        sys.exit(error)
+
+    path = getattr(module, "__path__", None)
+    if path is None:
+        sys.exit(f"No __path__ for {module}")
+    return path[0]
