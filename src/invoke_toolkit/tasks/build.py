109b95771bfdbc7bf88acb3dbda0671b87011093
Nahuel Defossé <D3f0@users.noreply.github.com>
2024-08-07 22:30:32 +0300

    Placeholder

--- src/invoke_toolkit/tasks/build.py
+++ src/invoke_toolkit/tasks/build.py
@@ -1,10 +1,13 @@
 """
-Generate self contained distributable scripts
+Generate self contained distributable script using some Python
+package for stand alone zipped code redistributables
 """
 
 from invoke import task, Context
+from invoke_toolkit.output import console
 
 
 @task(default=True)
 def build(ctx: Context):
     """Builds a package"""
+    console.print("[underline]Not yet implemented[/underline]")
