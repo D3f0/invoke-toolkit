6cdab544b78bdcec1e5dbf72c76e19db0f2e9c09
Nahuel Defossé <D3f0@users.noreply.github.com>
2025-10-14 15:15:18 +0300

    Move to uv-dynamic-versioning

--- tasks.py
+++ tasks.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+from typing import Annotated
 from invoke import task, Context
 import subprocess
 
@@ -6,6 +8,20 @@ REPO_ROOT = (
     .strip()
     .decode()
 )
+
+TOP_LEVEL: Annotated[Path, "The root directory of the repo"] = Path(__file__).parent
+
+
+@task(default=True, autoprint=True)
+def version(
+    ctx: Context,
+):
+    """Shows package version (git based)"""
+    with ctx.cd(TOP_LEVEL):
+        return ctx.run(
+            "uvx --with uv-dynamic-versioning hatchling version",
+            hide=not ctx.config.run.echo,
+        ).stdout.strip()
 
 
 @task()
