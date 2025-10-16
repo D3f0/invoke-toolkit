55103118dde282ba44277c2127090c161d25c58b
Nahuel Defossé <D3f0@users.noreply.github.com>
2025-10-16 10:55:33 +0300

    fix(invoke-context-usage-in-tests): Fix ctx fixture to prevent using in_stream
    Also updated fixtures to work for plugin tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,10 @@
+from pathlib import Path
+
 import pytest
-from invoke.context import Context
+
+# from invoke.context import Context
+from invoke_toolkit import Context
+from typing import Generator
 
 
 @pytest.fixture
@@ -7,9 +12,31 @@ def ctx() -> Context:
     """
     Returns invoke context
     """
-    return Context()
+    c = Context()
+    # Prevent using sys.stdin in pytest
+    c.config["run"]["in_stream"] = False
+    return c
 
 
 @pytest.fixture
 def git_root(ctx) -> str:
-    return ctx.run("git rev-parse --show-toplevel", in_stream=False).stdout.strip()
+    folder = Path(__file__).parent
+    return ctx.run(
+        f"git -C {folder} rev-parse --show-toplevel",
+    ).stdout.strip()
+
+
+@pytest.fixture
+def venv(ctx, tmp_path: Path) -> Generator[Path, None, None]:
+    """A virtual environment in a temporary directory"""
+    with ctx.cd(tmp_path):
+        ctx.run(
+            "uv venv",
+        )
+        yield tmp_path
+
+
+@pytest.fixture
+def package_in_venv(git_root, ctx: Context, venv: Path) -> None:
+    """A virtual environment in a temporary directory with the package"""
+    ctx.run(f"uv pip install --editable {git_root}")
